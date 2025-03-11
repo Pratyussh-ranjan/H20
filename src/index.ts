1b4f631ebd67b5bb00cb3656c2a3dd0137917d58
Pratyussh-ranjan <nikhel.raj.142892@gmail.com>
2025-03-11 16:33:15 +0530

    Puppet update 3

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,32 @@
-import { Hono } from 'hono'
-import { serve } from '@hono/node-server'
+import { Hono } from "hono";
+import { serve } from "@hono/node-server"
+
 const app = new Hono();
-
-const Reminders: string[] = [];
-
-app.get('/', (c) => {
-  return c.json({ message: 'Hello world' }, 200)
+app.get('/', (context) => {
+  return context.json({ message: "Hello world" }, 200);
 })
 
-app.get('/reminders', (context) => {
-  return context.json({ Reminders }, 200);
+app.get('/generate', (context) => {
+  const randnum = Math.random();
+  return context.json({ "randomNumbers": randnum }, 200);
 })
-app.post('/AddReminders', async (context) => {
-  const body = await context.req.json();
+app.get('/currentTime', (context) => {
+  const date = new Date();
+  return context.json({ "currentTime": date }, 200);
+})
+app.get('/environment', (context) => {
+  const NV = process.version;
+  const PT = process.platform;
+  return context.json({ "NodeVersion": NV, "Platform": PT })
+})
 
-  Reminders.push(body.reminders);
-  return context.json({ Reminders },201)
+app.get('/puppet', (context) => {
+  const name = context.req.query('name');
+  const residence = context.req.query('addr');
+  const school= context.req.query('school');
+
+  return context.json({ name: name, residence: residence, school: school }, 200);
 })
+
 serve(app);
-
 console.log('server is running on http://localhost:3000');
